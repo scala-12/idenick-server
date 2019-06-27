18bd1a8decced58fe1c6a83633e62a2af0d2f1b8
vlad <kalashnikov@ipoint.ru>
2019-06-27 16:57:21 +0800

    registrators intead registers

--- idenick_rest_api_v0/urls.py
+++ idenick_rest_api_v0/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r'organizations', OrganizationViewSet, basename='Organization')
 router.register(r'departments', DepartmentViewSet, basename='Department')
 router.register(r'employees', EmployeeViewSet, basename='Employee')
-router.register(r'organizations/(?P<organization_id>[0-9]+)/registers', RegisterViewSet, basename='Login')
+router.register(r'organizations/(?P<organization_id>[0-9]+)/registrators', RegisterViewSet, basename='Login')
 router.register(r'organizations/(?P<organization_id>[0-9]+)/controllers', ControllerViewSet, basename='Login')
 
 urlpatterns = router.urls
