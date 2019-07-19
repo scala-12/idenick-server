8fdd289fbac6b61c920921a3dc50989f74d2dcc4
vlad <kalashnikov@ipoint.ru>
2019-07-19 14:02:36 +0800

    guid for user

--- idenick_app/models.py
+++ idenick_app/models.py
@@ -121,6 +121,7 @@ class Login(models.Model):
     ]
     
     id = models.AutoField(primary_key=True)
+    guid = models.UUIDField(default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=4, choices=USER_ROLE, blank=True)
     organization = models.ForeignKey('Organization', on_delete=models.CASCADE, null=True, blank=True)
@@ -139,7 +140,7 @@ class Login(models.Model):
         return super(self.__class__, self).delete(*args, **kwargs)
     
     def __str__(self):
-        return self._str() + ('%s %s %s %s' % (self.organization, self.user.username, self.user.first_name + ' ' + self.user.last_name, self.get_role_display()))
+        return self._str() + ('guid[%s] %s %s %s %s' % (self.guid, self.organization, self.user.username, self.user.first_name + ' ' + self.user.last_name, self.get_role_display()))
 
 
 @receiver(post_save, sender=User)
